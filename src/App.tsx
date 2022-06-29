b97a31d57a3e022a227e3f47a3c417c357aea302
maxiang <maxiang@haoshuo.com>
2022-06-29 11:34:02 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn">123</button>
+    </div>
+  );
+};
+
+export default App;
